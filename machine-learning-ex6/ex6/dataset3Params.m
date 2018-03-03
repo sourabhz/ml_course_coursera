ba08d865b7e3f56a43de961ec5a0d8e931c239b3
Sourabh Zalke <zalkesourabh43@gmail.com>
2018-03-03 15:12:38 +0530

    Updating my original implementation

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,22 +23,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 steps = [ 0.01 0.03 0.1 0.3 1 3 10 30 ];
-minError = Inf;
+
 minC = Inf;
+minError = Inf;
 minSigma = Inf;
 
-for i = 1:length(steps)
-    for j = 1:length(steps)
-        currentC = steps(i);
-        currentSigma = steps(j);
-        model = svmTrain(X, y, currentC, @(x1, x2) gaussianKernel(x1, x2, currentSigma));
-        predictions = svmPredict(model, Xval);
+for i= 1:length(steps)
+    for j=1:length(steps)
+        C_ = steps(i);
+        Sigma = steps(j);
+        model = svmTrain(X,y,C_,@(x1,x2) gaussianKernel(x1,x2,Sigma));
+        predictions = svmPredict(model,Xval);
         error = mean(double(predictions ~= yval));
-
-        if error < minError
+        
+        if(error<minError)
             minError = error;
-            minC = currentC;
-            minSigma = currentSigma;
+            minC = C_;
+            minSigma = Sigma;
         end
     end
 end
